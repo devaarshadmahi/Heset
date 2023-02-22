1289d83c2492a3b4fccec3deb2d9c2d06d0ced87
aalverse <aalverses@gmail.com>
2023-02-22 17:29:10 -0500

    Added basic web controller for landing page

--- Authentication/src/main/java/com/dynatheia/heset/Authentication/AuthenticationApplication.java
+++ Authentication/src/main/java/com/dynatheia/heset/Authentication/AuthenticationApplication.java
@@ -2,8 +2,9 @@ package com.dynatheia.heset.Authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AuthenticationApplication {
 
 	public static void main(String[] args) {
