1472d2f928fb155dee36af156529387512822bd1
Raphael Getachew <rva.raph@gmail.com>
2023-03-01 16:32:46 -0500

    Fixed white list to only allow authentication/register requests

--- Authentication/src/main/java/com/dynatheia/heset/Authentication/config/SecurityConfig.java
+++ Authentication/src/main/java/com/dynatheia/heset/Authentication/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/**")
+                .requestMatchers("/api/v1/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
